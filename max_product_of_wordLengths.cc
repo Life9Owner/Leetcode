a6db5c901f26bb26dba6005a0b5b84c6090a9e5f
Life9Owner <1349615576@qq.com>
2023-11-07 16:30:47 +0800

    318

---
+++ max_product_of_wordLengths.cc
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int maxProduct(vector<string>& words) {
+        vector<int> bits(words.size(),0);
+        for(int i=0;i<words.size();i++)
+        {
+            for(int j=0;j<words[i].size();j++)
+            {
+                bits[i]|=1<<(words[i][j]-'a');
+            }
+        }
+        int res=0;
+        for(int i=0;i<words.size();i++)
+        {
+            for(int j=i+1;j<words.size();j++)
+            {
+                if((bits[i]&bits[j])==0)
+                {
+                    res=max(res,int(words[i].size()*words[j].size()));
+                }
+            }
+            
+        }
+        return res;
+    }
+};
